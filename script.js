a3452956ae1633cacd1594d50984583e1e7874bd
jorjo19fe <93598612+jorjo19fe@users.noreply.github.com>
2022-01-22 17:59:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var x="0"
+while(true){
+console.log (x) 
+document.write(x)
+x++
+}
+var y="0"
+while(true){
+console.log (y) 
+document.write(y)
+y++
+}
+var z="0"
+while(true){
+console.log (z) 
+document.write(z)
+z++
+}
+var q="0"
+while(true){
+console.log (q) 
+document.write(q)
+q++
+}
+var w="0"
+while(true){
+console.log (w) 
+document.write(w)
+w++
+}
